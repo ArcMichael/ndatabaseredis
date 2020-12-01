eab3bca94b8766f2833cbee623f454aa145b23bc
Michael <hulen20@gmail.com>
2020-12-01 17:26:29 +0800

    st

---
+++ start.sh
@@ -0,0 +1,1 @@
+docker exec -it redis7001 redis-cli -p 7001 -a UzE9M8JGL8I6 --cluster create 192.168.3.57:7001 192.168.3.57:7002 192.168.3.57:7003 192.168.3.57:7004 192.168.3.57:7005 192.168.3.57:7006 --cluster-replicas 1
