c96d04f959c1d4e82e864dd3fd682760e20d39bd
Michael <hulen20@gmail.com>
2021-02-25 12:52:20 +0800

    op

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 # systemctl status firewalld.service
 # systemctl stop firewalld
 
+# sh start.sh
+
 docker exec -it redis7001 redis-cli -p 7001 -a UzE9M8JGL8I6 --cluster create 192.168.3.201:7001 192.168.3.201:7002 192.168.3.201:7003 192.168.3.201:7004 192.168.3.201:7005 192.168.3.201:7006 --cluster-replicas 1
 
 # test
-docker exec -ti redis7991 redis-cli -h 192.168.3.201 -p 7003 -a UzE9M8JGL8I6 -c
+# docker exec -ti redis7991 redis-cli -h 192.168.3.201 -p 7003 -a UzE9M8JGL8I6 -c
