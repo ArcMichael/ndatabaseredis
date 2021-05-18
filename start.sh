93f4de4c767c7b6e4ed2e31bdae35371421200e9
Michael <hulen20@gmail.com>
2021-05-18 15:34:46 +0800

    Update start.sh
    1

--- start.sh
+++ start.sh
@@ -12,6 +12,8 @@ docker exec -it redis7001 redis-cli -p 7001 -a UzE9M8JGL8I6 --cluster create 192
 # 1. r m -r f 7001 7002 7003 7004 7005 7006
 # 2 ./redis-cluster-config.sh
 # 3. systemctl stop firewalld
-# 4. docker-compose -f docker-compose-redis-cluster.yml up -d
-# 5. docker exec -it redis7001 redis-cli -p 7001 -a UzE9M8JGL8I6 --cluster create 192.168.3.201:7001 192.168.3.201:7002 192.168.3.201:7003 192.168.3.201:7004 192.168.3.201:7005 192.168.3.201:7006 --cluster-replicas 1
-# 6. yes
+# 4. sudo iptables -F
+# 5. service docker restart
+# 6. docker-compose -f docker-compose-redis-cluster.yml up -d
+# 7. docker exec -it redis7001 redis-cli -p 7001 -a UzE9M8JGL8I6 --cluster create 192.168.3.201:7001 192.168.3.201:7002 192.168.3.201:7003 192.168.3.201:7004 192.168.3.201:7005 192.168.3.201:7006 --cluster-replicas 1
+# 8. yes
